ae7c8431788100151cb9528ffe9c334718ec45fb
Charles.Oliveira <86701044+Cha-Oliveira@users.noreply.github.com>
2025-04-02 23:23:30 -0300

    Status/Specialty

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,5 +4,25 @@ export const routes = [
     name: 'dashboard',
     component: () =>
       import(/* webpackChunkName: "dashboard" */ '@/pages/dashboard/DashboardPage.vue')
+  },
+  {
+    path: '/status',
+    name: 'status-list',
+    component: () => import(/* webpackChunkName: "status" */ '@/pages/status/StatusListPage.vue')
+  },
+  {
+    path: '/status/cadastrar',
+    name: 'status-insert',
+    component: () => import(/* webpackChunkName: "status" */ '@/pages/status/StatusFormPage.vue')
+  },
+  {
+    path: '/specialty',
+    name: 'specialty-list',
+    component: () => import(/* webpackChunkName: "specialty" */ '@/pages/specialty/SpecialtyListPage.vue')
+  },
+  {
+    path: '/specialty/cadastrar',
+    name: 'specialty-insert',
+    component: () => import(/* webpackChunkName: "specialty" */ '@/pages/specialty/SpecialtyFormPage.vue')
   }
 ]
